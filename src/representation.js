00996f1f9afcbdb59839fe5f9a3d966638d47b89
michaeljosephrosenthal <michael.the.awesome@gmail.com>
2016-03-23 22:52:22 -0500

    error free isomorphism

--- src/representation.js
+++ src/representation.js
@@ -4,8 +4,10 @@ export default ddServer.implement({
     name: 'DomainDrivenExpress',
     constructor({ Domains: domains, middlewareGenerators=[], server={} }){
         server._domains = server._domains || {}
-        server.generateMiddleware = (domains) => server
+        server.generateMiddleware = function(domains) { return this }
         server.generateMiddleware.bind(server)(domains)
+        server.use = function() { return Error("express requires a node context") }
+        server.listen = function() { return Error("express requires a node context") }
         return [server]
     },
     provider(){ return this }
